7ed086193eacc8ca060f9d5f6d9789a00afc2d06
Soma Gallai <soma.gallai@cm-robotics.com>
2021-07-21 13:20:28 +0200

    cherry picked features added from add_install_docker_script branch to docker2 branch

--- docker/bin/set-me-up.sh
+++ docker/bin/set-me-up.sh
@@ -6,7 +6,7 @@ echo "Docker container $container_id"
 echo "Write absolute path of the folder on your computer, you want the work files folders ( /home/ros and /usr/local/lib/python3.8/dist-packages ) on the docker machine to be copied into:"
 read destination_folder
 mkdir -p $destination_folder/home/ros
-mkdir -p $destination_folder/usr/local/lib/python3.8/dist-packages
+mkdir -p $destination_folder/usr/local
 docker cp $container_id:/home/ros $destination_folder/home/ros
-docker cp $container_id:/usr/local/lib/python3.8/dist-packages $destination_folder/usr/local/lib/python3.8/dist-packages
+docker cp $container_id:/usr/local $destination_folder/usr/local
 docker stop -t 1 $container_id
